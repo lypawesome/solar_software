4e5d881bb4b9db35402badbdb5d44755113f0ec9
shinebird <949232047@qq.com>
2023-06-18 19:50:01 +0800

    新增getParaPtr（直接获取成员变量的指针）

--- solar_2/source/utils/ParaUtils.h
+++ solar_2/source/utils/ParaUtils.h
@@ -34,8 +34,9 @@ namespace solar
             {
                 if (!found && D.name == property_type)
                 {
-                    qDebug() <<"D.name="<<D.name<<"  property_type="<<QString::fromStdString(property_type);
-                    
+                    // qDebug() << "D.name=" << D.name
+                    //          << "  property_type=" << QString::fromStdString(property_type);
+
                     found = true;
                     *object.*D.pointer =
                         value.value<std::remove_reference_t<decltype(*object.*D.pointer)>>();
@@ -66,7 +67,7 @@ namespace solar
                 {
                     found = true;
                     ret.setValue(*object.*D.pointer);
-                    //qDebug() <<"----------*object.*D.pointer="<<*object.*D.pointer;
+                    // qDebug() <<"----------*object.*D.pointer="<<*object.*D.pointer;
                 }
             });
         if (!found) [[unlikely]]
@@ -77,7 +78,7 @@ namespace solar
             throw ::solar::format("ERROR: {}::getPara(): No such property: {}", NamedClass::name(),
                                   property_type);
         }
-        //qDebug()<<"----------- ret = "<<ret;
+        // qDebug()<<"----------- ret = "<<ret;
         return ret;
     }
 
@@ -122,6 +123,33 @@ namespace solar
                 {
                     found = true;
                     ret = *object.*D.pointer;
+                }
+            });
+        if (!found) [[unlikely]]
+        {
+            qDebug() << QString(::solar::format("ERROR: {}::getPara(): No such property: {}",
+                                                NamedClass::name(), property_type)
+                                    .c_str());
+            throw ::solar::format("ERROR: {}::getPara(): No such property: {}", NamedClass::name(),
+                                  property_type);
+        }
+        return ret;
+    }
+
+    template <ClassHasName NamedClass>
+    [[nodiscard]] auto getParaPtr(const std::string& property_type, NamedClass* object) -> std::any
+    {
+        std::any ret;
+        using D1 = boost::describe::describe_members<std::remove_reference_t<NamedClass>,
+                                                     boost::describe::mod_any_access>;
+        bool found = false;
+        boost::mp11::mp_for_each<D1>(
+            [&](auto&& D)
+            {
+                if (!found && D.name == property_type)
+                {
+                    found = true;
+                    ret = std::addressof(*object.*D.pointer);
                 }
             });
         if (!found) [[unlikely]]
